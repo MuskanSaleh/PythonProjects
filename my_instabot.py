f80d441268686dba881fea0f83004a3fc05e8b6f
Muskan <muskaankorejo66@gmail.com>
2025-02-03 02:19:07 +0500

    changes made

--- my_instabot.py
+++
@@ -1,7 +0,0 @@
-#we can upload photos follow unfollow
-from instabot import Bot
-from PIL import Image
-
-bot = Bot()
-bot.login(username = "sanahere68",password = "A@li1983")
-bot.follow('asad.panhwar05')
